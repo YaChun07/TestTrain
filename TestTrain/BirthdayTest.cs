50466837d9e80c0ba39bc7f97f0a6aa08429ffa6
amanda.yang93 <amanda.yang@titansoft.com.sg>
2018-07-28 10:15:37 +0800

    Extract Holiday and finish isTodayJoeyBirthday

---
+++ TestTrain/BirthdayTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestTrain
+{
+    [TestClass]
+    public class BirthdayTest
+    {
+        [TestMethod]
+        public void IsTodayNotJoeyBirthdayTest()
+        {
+            var holiday = new FakeHoliday();
+            holiday.SetToday(new DateTime(2018,07,28));
+
+            Assert.AreEqual("No", holiday.IsTodayJoeyBirthday());
+        }
+
+        [TestMethod]
+        public void IsTodayJoeyBirthdayTest()
+        {
+            var holiday = new FakeHoliday();
+            holiday.SetToday(new DateTime(2018, 09, 01));
+
+            Assert.AreEqual("Happy Birthday", holiday.IsTodayJoeyBirthday());
+        }
+    }
+
+    public class FakeHoliday : Holiday
+    {
+        private DateTime _today;
+        internal override DateTime GetToday()
+        {
+            return _today;
+        }
+
+        internal void SetToday(DateTime date)
+        {
+            _today = date;
+        }
+    }
+}
---
+++ TestTrain/Holiday.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TestTrain
+{
+    public class Holiday
+    {
+        public string IsTodayJoeyBirthday()
+        {
+            var today = GetToday();
+
+            if (today.Month == 9 && today.Day == 1)
+            {
+                return "Happy Birthday";
+            }
+            return "No";
+        }
+
+        internal virtual DateTime GetToday()
+        {
+            return DateTime.Today;
+        }
+    }
+}
