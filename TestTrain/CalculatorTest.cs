b555de4108d307a6e7c72c4f094cb6d87454f8f4
amanda.yang93 <amanda.yang@titansoft.com.sg>
2018-07-28 09:25:49 +0800

    Add first test for calculator

---
+++ TestTrain/CalculatorTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestTrain
+{
+    [TestClass]
+    public class CalculatorTest
+    {
+        [TestMethod]
+        public void add_two_positive_int()
+        {
+            var calculator = new Calculator();
+            var first = 1;
+            var second = 2;
+            var result =calculator.Add(first, second);
+            Assert.AreEqual(3, result);
+        }
+
+        public class Calculator
+
+        {
+            public int Add(int first, int second)
+            {
+                return first + second;
+            }
+        }
+    }
+}
